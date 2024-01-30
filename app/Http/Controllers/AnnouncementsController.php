804f098ab6eb36dc3b1d248dfbb2a5acb5f13bcb
Yasser <aitelghariyasser@gmail.com>
2024-01-30 20:13:23 +0100

    adding the auth using breeze package and blade templeting also fix some other bugs*

---
+++ app/Http/Controllers/AnnouncementsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\announcements;
+use App\Http\Requests\StoreannouncementsRequest;
+use App\Http\Requests\UpdateannouncementsRequest;
+
+class AnnouncementsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreannouncementsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(announcements $announcements)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(announcements $announcements)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateannouncementsRequest $request, announcements $announcements)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(announcements $announcements)
+    {
+        //
+    }
+}
